416dc585586e8b2d73ce37de144f7b61ba7cbcc2
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-27 17:01:11 +0100

    funzione che somma i numeri

--- Pari o Dispari/js/script.js
+++ Pari o Dispari/js/script.js
@@ -12,19 +12,30 @@ genera.addEventListener('click', function(){
 
     //definisco variabile scelta utente e numero selezionato
     const userChoice = document.getElementById('choice').value;
-    console.log(userChoice);
+
     const userNum = document.getElementById('numero').value;
-    console.log(userNum); 
     // funzione numero casuale pc
     function pcNum(min, max){
         let num = Math.floor(Math.random(min) * max) + min;
         return num
     }
+    // definisco variabile per il numero scelto dal pc
+    let pcChoice = parseInt(pcNum(1,5));
+    console.log('il numero scelto dla pc è: ' + pcChoice);    
     // visualizzo su console scelta user
     console.log('La tua scelta é: ' + userChoice);
     console.log('Il numero che hai scelto è: ' + userNum);
     
-
+    //funzione che somma i numeri
+    function sum(pcChoice, userNum) {
+        let risultato = pcChoice + userNum;
+        return risultato;
     }
 
+    let total = sum(pcChoice, userNum);
+
+    console.log('La somma delle scelte è: ' + total);
+
+    //dichiaro il riusltato dsella somma in una variabile
+ }
 );
