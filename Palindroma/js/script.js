7868710cc2b414081acdc786d55a7bd8f8241012
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-27 15:42:02 +0100

    definizione bottone js

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -2,3 +2,6 @@
 /* 1-   Chiedere all’utente di inserire una parola
    2-   Creare una funzione per capire se la parola inserita è palindroma
 */
+// definisco il bottone
+
+const checkButton = document.getElementById('send');
